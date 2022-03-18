f87aeda77898109fc781dc34a434a03255288970
tanishkul <tanishkul@gmail.com>
2022-03-18 20:31:08 +0530

    changes

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,9 @@ FROM node:alpine
 WORKDIR /usr/nodeapp
 
 # current directory(first ./) to target/working directory(second ./)
-COPY ./ ./
+COPY ./package.json ./
 
 RUN npm install
+COPY ./ ./
 
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Visiting Root</h1>');
+    res.json({
+        message: 'You are visiting root'
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.json({
+        message: 'You are visiting login root'
+    });
+});
+
+app.get('/signup', (req, res) => {
+    res.json({
+        message: 'You are visiting signup root'
+    });
 });
 
 app.listen(9000, () => {
